1bbe5affa6d8203ad6167b1eda70cb6076212590
teeqson <145769335+teeqson@users.noreply.github.com>
2024-01-24 16:56:22 +0100

    Change Picture, several changes, insert qualifications

--- js/script.js
+++ js/script.js
@@ -4,3 +4,24 @@ const navToggle = document.querySelector(".navbar-toggle");
 navToggle.addEventListener("click", function () {
   document.querySelector(".portfolio-navbar").classList.toggle("show");
 });
+
+
+// Tab Interface For Resume
+
+const resumeHeading = document.querySelector(".resume-heading");
+const resumeTabs = document.querySelectorAll(".resume-tab");
+
+resumeHeading.onclick = (event) => {
+  event.preventDefault();
+  const clickedItemId = event.target.dataset.id;
+  if (clickedItemId) {
+    resumeHeading.querySelector(".active").classList.remove("active");
+    event.target.classList.add("active");
+
+    resumeTabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    const correspondingTab = document.getElementById(clickedItemId);
+    correspondingTab.classList.add("active");
+  }
+};
