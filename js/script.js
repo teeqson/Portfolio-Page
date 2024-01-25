ed4cab18720d85b55fc2755b954de7c2ca34987c
teeqson <145769335+teeqson@users.noreply.github.com>
2024-01-25 16:51:02 +0100

    Added Hobbies Section, Insert Pictures, added Dark-Mode, scrollbar, smooth transitions.

--- js/script.js
+++ js/script.js
@@ -73,3 +73,102 @@ tabs.forEach(tab => {
 
 // Initialize tabs on page load
 document.addEventListener('DOMContentLoaded', initTabs);
+
+
+// Active Link On Page Scroll
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollTracker() {
+  const currentYScroll = window.scrollY;
+
+  sections.forEach((section) => {
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop - 100;
+    const id = section.getAttribute("id");
+    const currentNavLink = document.querySelector(
+      `header .portfolio-navbar a[href*="#${id}"]`
+    );
+    if (
+      currentYScroll > sectionTop &&
+      currentYScroll <= sectionTop + sectionHeight
+    ) {
+      currentNavLink.classList.add("active-link");
+    } else {
+      currentNavLink.classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollTracker);
+
+
+// Dark and Light Theme Toggle
+
+function isLight() {
+  return localStorage.getItem("dark-mode");
+}
+
+function toggleRootClass() {
+  document.querySelector("body").classList.toggle("dark");
+}
+
+function toggleLocalStorageItem() {
+  if (isLight()) {
+    localStorage.removeItem("dark-mode");
+  } else {
+    localStorage.setItem("dark-mode", "set");
+  }
+}
+
+if (isLight()) {
+  toggleRootClass();
+}
+
+document.querySelector(".theme-toggle").addEventListener("click", () => {
+  toggleLocalStorageItem();
+  toggleRootClass();
+});
+
+
+//Scroll Reveal
+
+const sr = ScrollReveal({
+  reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(".about-intro", {
+  origin: "left",
+});
+sr.reveal(
+  ".resume-heading,.resume-text,.service-row,.portfolio-wrapper,.contact-general,#contact-form",
+  {
+    origin: "bottom",
+  }
+);
+sr.reveal(".resume-body", {
+  origin: "top",
+});
+
+// Portfolio Filter
+
+const filterContainer = document.querySelector(".portfolio-filter-nav");
+const galleryItems = document.querySelectorAll(".portfolio-item");
+
+filterContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+  filterContainer.querySelector(".active").classList.remove("active");
+  e.target.classList.add("active");
+  const filterValue = e.target.getAttribute("data-id");
+  galleryItems.forEach((item) => {
+    if (item.classList.contains(filterValue) || filterValue === "all") {
+      item.classList.remove("hide");
+      item.classList.add("show");
+    } else {
+      item.classList.remove("show");
+      item.classList.add("hide");
+    }
+  });
+});
