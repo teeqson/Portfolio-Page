5c0eafe6890a5f5d4afa16e048c7d99673419552
teeqson <145769335+teeqson@users.noreply.github.com>
2024-01-25 05:27:34 +0100

    Footer, some fixing, contact-section

--- js/script.js
+++ js/script.js
@@ -25,3 +25,51 @@ resumeHeading.onclick = (event) => {
     correspondingTab.classList.add("active");
   }
 };
+
+// Navbar Header Sticky While Scroll
+
+function stickyNav() {
+  var headerHeight = document.querySelector("#about").offsetHeight / 2;
+  var navbar = document.querySelector("header");
+  var scrollValue = window.scrollY;
+
+  if (scrollValue > headerHeight) {
+    navbar.classList.add("header-sticky");
+  } else if (scrollValue < headerHeight) {
+    navbar.classList.remove("header-sticky");
+  }
+}
+
+window.addEventListener("scroll", stickyNav);
+
+
+// Selecting all tab buttons and content
+var tabs = document.querySelectorAll('.btn');
+var contents = document.querySelectorAll('.resume-tab');
+
+// Function to remove active class from tabs and hide all content
+function resetActiveContent() {
+    tabs.forEach(tab => tab.classList.remove('active'));
+    contents.forEach(content => content.classList.remove('active'));
+}
+
+// Function to initialize default active tab and content
+function initTabs() {
+    resetActiveContent();
+    // Set the default active tab here, e.g., Education
+    document.querySelector('.btn[data-id="education"]').classList.add('active');
+    document.querySelector('#education').classList.add('active');
+}
+
+// Event listener for tab buttons
+tabs.forEach(tab => {
+    tab.addEventListener('click', function() {
+        resetActiveContent();
+        var targetId = tab.getAttribute('data-id');
+        tab.classList.add('active');
+        document.getElementById(targetId).classList.add('active');
+    });
+});
+
+// Initialize tabs on page load
+document.addEventListener('DOMContentLoaded', initTabs);
